1c7c3fde4934a45acc3e49ff82adc026a27e0e07
Swapnil Bagdane <swapnilbagdane59@gmail.com>
2023-10-29 14:43:51 +0530

    Design a ResearchCards Section

---
+++ src/components/ResearchCards/index.js
@@ -0,0 +1,238 @@
+/*eslint-disable*/
+import {AiOutlineRight} from 'react-icons/ai'
+
+import './index.css'
+
+const ResearchCards = () => (
+  <>
+    <div className="main-card-container">
+      <div className="sub-card-container">
+        <div className="initial-container">
+          <div className="outer-container">
+            <h1 className="card-heading ">PODCAST</h1>
+            <h1 className="card-title">
+              Built for Change: Biotech, space and innovation’s big bang
+            </h1>
+            <div className="image-container">
+              <img
+                alt="card-img"
+                className="card-image"
+                src="https://dynamicmedia.accenture.com/is/image/accenture/Tech-Vision-Image-B-W:rad-card-portrait?ts=1692800281500&fit=constrain&dpr=off"
+              />
+            </div>
+          </div>
+          <div className="inner-container description-container">
+            <h1 className="para color card-description">
+              In this episode, we explore the nascent fields of space data and
+              synthetic biology and identify key strategies for enterprises to
+              benefit from thi science-technology innovation feedback loop.
+            </h1>
+            <p className="card-button">
+              Listen Now <AiOutlineRight />
+            </p>
+          </div>
+        </div>
+        <div className="initial-container-1">
+          <div className="outer-container">
+            <h1 className="card-heading-1 ">RESEARCH REPORT</h1>
+            <h1 className="card-title-1">
+              Leading with edge computing: How to reinvent with data & AI
+            </h1>
+            <div className="image-container-1">
+              <h1 className="percent">83%</h1>
+              <h1 className="img-description">
+                of companies believe edge <br />
+                computing is essential to future <br />
+                competitiveness
+              </h1>
+            </div>
+          </div>
+          <div className="inner-container-1 description-container">
+            <h1 className="card-description-1">
+              Edge enables the rapid processing of data close to its source and
+              plays a key role in evolving AI for the enterprise. Our research
+              reveals how companies across the globe are using edge, and how
+              they can scale its value.
+            </h1>
+            <p className="card-button-1">
+              Expand <AiOutlineRight />
+            </p>
+          </div>
+        </div>
+        <div className="initial-container-2">
+          <div className="outer-container">
+            <h1 className="card-heading-1 ">RESEARCH REPORT</h1>
+            <h1 className="card-title-1">Strategy at the pace of technology</h1>
+            <div className="image-container-2">
+              <h1 className="percent">21%</h1>
+              <h1 className="card-description-1">
+                of companies are leveraging technology to shape business
+                strategy
+              </h1>
+            </div>
+          </div>
+          <div className="inner-container-1 description-container">
+            <h1 className="card-description-1">
+              Technology is increasing the complexity and speed of business
+              strategy. Separating noise from relevance is hard. A cohort of
+              leaders are outperforming their peers by using tech to both inform
+              and execute on their strategy.
+            </h1>
+            <p className="card-button-1">
+              Expand <AiOutlineRight />
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="sub-card-container">
+        <div className="initial-container-3">
+          <div className="outer-container">
+            <h1 className="card-heading-1 ">RESEARCH REPORT</h1>
+            <h1 className="card-title-1">
+              Forge the link between tech investment and business value
+            </h1>
+            <div className="image-container-3">
+              <h1 className="percent">90%</h1>
+              <h1 className="card-description-1">
+                of CxOs say they plan to increase their technology spending in
+                the year ahead
+              </h1>
+            </div>
+          </div>
+          <div className="inner-container-1 description-container">
+            <h1 className="card-description-1">
+              This is a singular moment for CIOs: here is how they can take
+              advantage to unlock true business value across their enterprise.
+            </h1>
+            <p className="card-button-1">
+              Expand <AiOutlineRight />
+            </p>
+          </div>
+        </div>
+        <div className="initial-container-4">
+          <div className="outer-container">
+            <h1 className="card-heading ">RESEARCH REPORT</h1>
+            <h1 className="card-title">
+              Five practices to thrive on the Cloud Continuum
+            </h1>
+          </div>
+          <div className="inner-container-1 description-container">
+            <h1 className="card-description-1 carrd-heading">
+              Accenture details 5 cloud transformation practices to help
+              companies extract maximum value from cloud and succeed in the
+              Cloud Continuum.
+            </h1>
+            <p className="card-button">
+              Expand <AiOutlineRight />
+            </p>
+          </div>
+        </div>
+        <div className="initial-container-5">
+          <div className="outer-container">
+            <h1 className="card-heading-1 carrd-heading">RESEARCH REPORT</h1>
+            <h1 className="card-title-1 carrd-heading">
+              Positive energy in the metaverse. A new era of efficiency
+            </h1>
+            <div className="image-container-5">
+              <h1 className="percent carrd-heading">72%</h1>
+              <h1 className="card-description-1 carrd-heading">
+                of executives in the energy industry believe that primary
+                metaverse technologies are already inspiring their
+                organization’s vision or long-term strategy
+              </h1>
+            </div>
+          </div>
+          <div className="inner-container-1 description-container">
+            <h1 className="card-description-1 carrd-heading">
+              The world around us has undergone a transformation, and the energy
+              industry has embraced this metamorphosis to emerge safer, more
+              efficient and focused on net- zero ambitions.
+            </h1>
+            <p className="card-button">
+              Expand <AiOutlineRight />
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="sub-card-container">
+        <div className="initial-container-6">
+          <div className="outer-container">
+            <h1 className="card-heading-1 carrd-heading ">CASE STUDY</h1>
+            <h1 className="card-title-1 carrd-heading">
+              MQDC: Bringing wellbeing to the metaverse
+            </h1>
+          </div>
+          <div className="inner-container-1 description-container">
+            <h1 className="card-description-1 carrd-heading ">
+              With a virtual universe that connects an online land of creativity
+              to physical properties—real estate developer MQDC is bringing the
+              real and digital worlds together—and rewarding positive actions
+              for real-world impact.
+            </h1>
+            <p className="card-button">
+              Expand <AiOutlineRight />
+            </p>
+          </div>
+        </div>
+        <div className="initial-container-7">
+          <div className="outer-container">
+            <h1 className="card-heading-1 ">PERSPECTIVE</h1>
+            <h1 className="card-title-1">AI for everyone</h1>
+            <div className="image-container-7">
+              <h1 className="percent">40%</h1>
+              <h1 className="card-description-1">
+                of all working hours can be impacted by Large language models
+                like GPT-4.
+              </h1>
+            </div>
+          </div>
+          <div className="inner-container-1 description-container">
+            <h1 className="card-description-1">
+              With innovations like ChatGPT dominating headlines, the world is
+              waking up to the transformative potential of generative AI. Find
+              out how AI working side-by-side with people will impact science,
+              business and society itself.
+            </h1>
+            <p className="card-button-1">
+              Expand <AiOutlineRight />
+            </p>
+          </div>
+        </div>
+        <div className="initial-container-8">
+          <div className="outer-container">
+            <h1 className="card-heading ">RESEARCH REPORT</h1>
+            <h1 className="card-title">
+              State of Cybersecurity Resilience 2023
+            </h1>
+            <div className="image-container-8">
+              <h1 className="percent carrd-heading">5.8X</h1>
+              <h1 className="card-description-1 carrd-heading">
+                more likely that cyber transformers have more effective digital
+                transformations.
+              </h1>
+            </div>
+          </div>
+          <div className="inner-container-1 description-container">
+            <h1 className="card-description-1 carrd-heading">
+              Cyber transformers are excelling at both business resilience and
+              business outcomes—with high-performing cybersecurity propelling
+              their digital transformations forward.
+            </h1>
+            <p className="card-button">
+              Expand <AiOutlineRight />
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="degree-container">
+      <h1 className="value-heading">360° VALUE</h1>
+      <h1 className="value-description">
+        Every day, we embrace change and create value for all our stakeholders,
+        in every part of the world.
+      </h1>
+    </div>
+  </>
+)
+
+export default ResearchCards
